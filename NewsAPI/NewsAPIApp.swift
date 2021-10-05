70f05596c4993f9474d9797365cc434a484413f2
Rob <maltese.rob@gmail.com>
2021-10-05 17:44:55 -0400

    Initial Commit

---
+++ NewsAPI/NewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPIApp.swift
+//  NewsAPI
+//
+//  Created by Rob Maltese on 10/5/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
